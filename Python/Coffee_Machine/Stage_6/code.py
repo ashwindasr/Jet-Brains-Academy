5472b2bb4e027e97dd966604f2c7bc9ef35d4fce
Ashwin Das <ashwindasr@users.noreply.github.com>
2020-05-08 02:14:29 +0530

    Update code.py

--- Python/Coffee_Machine/Stage_6/code.py
+++ Python/Coffee_Machine/Stage_6/code.py
@@ -1,61 +1,62 @@
 # Write your code here
 class CoffeeMachine:
-    water_current = 400
-    milk_current = 540
-    coffee_current = 120
-    money_current = 550
-    cups_current = 9
-    state = None
-    fill_state = "water"
 
-    def __init__(self):
+    def __init__(self, water_current, milk_current, coffee_current, money_current, cups_current):
+        self.water_current = int(water_current)
+        self.milk_current = milk_current
+        self.coffee_current = coffee_current
+        self.money_current = money_current
+        self.cups_current = cups_current
+        self.state = None
+        self.fill_state = "water"
+        
         print("Write action (buy, fill, take, remaining, exit):\n")
 
     def console(self, input_):
-        if CoffeeMachine.state is None:
-            CoffeeMachine.state = "choosing a state"
+        if self.state is None:
+            self.state = "choosing a state"
             if input_ == "buy":
                 print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")
-                CoffeeMachine.state = "choosing a coffee"
+                self.state = "choosing a coffee"
             elif input_ == "fill":
                 print("Write how many ml of {CoffeeMachine.fill_state} do you want to add:\n")
-                CoffeeMachine.state = "fill"
+                self.state = "fill"
             elif input_ == "take":
                 self.take()
-                CoffeeMachine.state = None
+                self.state = None
                 print("Write action (buy, fill, take, remaining, exit):\n")
             elif input_ == "remaining":
                 self.remaining()
-                CoffeeMachine.state = None
+                self.state = None
                 print("Write action (buy, fill, take, remaining, exit):\n")
             else:
                 return True
 
-        elif CoffeeMachine.state == "choosing a coffee":
+        elif self.state == "choosing a coffee":
             self.buy(input_)
-            CoffeeMachine.state = None
+            self.state = None
             print("Write action (buy, fill, take, remaining, exit):\n")
-        elif CoffeeMachine.state == "fill":
-            if CoffeeMachine.fill_state == "water":
+        elif self.state == "fill":
+            if self.fill_state == "water":
                 self.fill("water", input_)
-                CoffeeMachine.fill_state = "milk"
+                self.fill_state = "milk"
                 print("Write how many ml of {CoffeeMachine.fill_state} do you want to add:\n")
-            elif CoffeeMachine.fill_state == "milk":
+            elif self.fill_state == "milk":
                 self.fill("milk", input_)
-                CoffeeMachine.fill_state = "coffee beans"
+                self.fill_state = "coffee beans"
                 print("Write how many grams of {CoffeeMachine.fill_state} do you want to add:\n")
-            elif CoffeeMachine.fill_state == "coffee beans":
+            elif self.fill_state == "coffee beans":
                 self.fill("coffee beans", input_)
-                CoffeeMachine.fill_state = "cups"
+                self.fill_state = "cups"
                 print("Write how many disposable cups of coffee do you want to add:\n")
             else:
                 self.fill("cups", input_)
-                CoffeeMachine.fill_state = "water"
-                CoffeeMachine.state = None
+                self.fill_state = "water"
+                self.state = None
 
     def take(self):
         print("I gave you ${}".format(self.money_current))
-        CoffeeMachine.money_current = 0
+        self.money_current = 0
 
     def buy(self, choice):
         resources = False
@@ -63,58 +64,58 @@ class CoffeeMachine:
             return
         choice = int(choice)
         if choice == 1:
-            if not (CoffeeMachine.water_current >= 250):
+            if not (self.water_current >= 250):
                 print("Sorry, not enough water!")
-            elif not (CoffeeMachine.coffee_current >= 16):
+            elif not (self.coffee_current >= 16):
                 print("Sorry, not enough coffee beans!")
             else:
                 print("I have enough resources, making you a coffee!")
-                CoffeeMachine.water_current -= 250
-                CoffeeMachine.coffee_current -= 16
-                CoffeeMachine.money_current += 4
+                self.water_current -= 250
+                self.coffee_current -= 16
+                self.money_current += 4
                 resources = True
         elif choice == 2:
-            if not (CoffeeMachine.water_current >= 350):
+            if not (self.water_current >= 350):
                 print("Sorry, not enough water!")
-            elif not (CoffeeMachine.milk_current >= 75):
+            elif not (self.milk_current >= 75):
                 print("Sorry, not enough milk!")
-            elif not (CoffeeMachine.coffee_current >= 20):
+            elif not (self.coffee_current >= 20):
                 print("Sorry, not enough coffee beans!")
             else:
                 print("I have enough resources, making you a coffee!")
-                CoffeeMachine.water_current -= 350
-                CoffeeMachine.milk_current -= 75
-                CoffeeMachine.coffee_current -= 20
-                CoffeeMachine.money_current += 7
+                self.water_current -= 350
+                self.milk_current -= 75
+                self.coffee_current -= 20
+                self.money_current += 7
                 resources = True
         else:
-            if not (CoffeeMachine.water_current >= 100):
+            if not (self.water_current >= 100):
                 print("Sorry, not enough water!")
-            elif not (CoffeeMachine.milk_current >= 75):
+            elif not (self.milk_current >= 75):
                 print("Sorry, not enough milk!")
-            elif not (CoffeeMachine.coffee_current >= 12):
+            elif not (self.coffee_current >= 12):
                 print("Sorry, not enough coffee beans!")
             else:
                 print("I have enough resources, making you a coffee!")
-                CoffeeMachine.water_current -= 200
-                CoffeeMachine.milk_current -= 100
-                CoffeeMachine.coffee_current -= 12
-                CoffeeMachine.money_current += 6
+                self.water_current -= 200
+                self.milk_current -= 100
+                self.coffee_current -= 12
+                self.money_current += 6
                 resources = True
         if resources:
-            CoffeeMachine.cups_current -= 1
+            self.cups_current -= 1
 
     def fill(self, type_, input_):
         input_ = int(input_)
 
         if type_ == "water":
-            CoffeeMachine.water_current += input_
+            self.water_current += input_
         elif type_ == "milk":
-            CoffeeMachine.milk_current += input_
+            self.milk_current += input_
         elif type_ == "coffee beans":
-            CoffeeMachine.coffee_current += input_
+            self.coffee_current += input_
         else:
-            CoffeeMachine.cups_current += input_
+            self.cups_current += input_
 
     def remaining(self):
         print("The coffee machine has:")
@@ -125,7 +126,7 @@ class CoffeeMachine:
         print("$" + str(self.money_current) + " of money")
 
 
-machine = CoffeeMachine()
+machine = CoffeeMachine(400, 540, 120, 550, 9)
 while True:
     input_ = input()
     if machine.console(input_):
